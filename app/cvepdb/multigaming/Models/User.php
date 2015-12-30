13ce8ecdab1c784e1dedd36df08c731fc467f3f3
Antoine Benevaut <antoine@cvepdb.fr>
2015-12-30 14:23:02 +0100

    Avancement general.

--- app/cvepdb/multigaming/Models/User.php
+++ app/cvepdb/multigaming/Models/User.php
@@ -4,8 +4,21 @@ namespace App\CVEPDB\Multigaming\Models;
 
 use App\User as UserBaseModel;
 
+use App\CVEPDB\Multigaming\Traits\Models\LogTrait;
+
 class User extends UserBaseModel
 {
+    use LogTrait;
+
+    /**
+     * @var array LogTrait property
+     */
+    protected static $recordEvents = [
+        'created',
+        'updated',
+        'deleted',
+    ];
+
     /**
      * The teams that belong to the user.
      */
