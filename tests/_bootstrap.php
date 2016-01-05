2cdce557baca4a6f9018972327c239c6331bbb97
Antoine Benevaut <antoine@cvepdb.fr>
2016-01-05 18:09:07 +0100

    Avancement sur les tests unitaires.

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,14 +1,16 @@
 <?php
 // This is global bootstrap for autoloading
 
-
 require 'bootstrap/autoload.php';
 $app = require 'bootstrap/app.php';
+
 $app->loadEnvironmentFrom('.env.testing');
 $app->instance('request', new \Illuminate\Http\Request);
 $app->make('Illuminate\Contracts\Http\Kernel')->bootstrap();
 
 
+exec('rm ' . storage_path(env('SQLITE_DB_PATH')));
+exec('cp '.storage_path('databases/database.sqlite').' '.storage_path(env('SQLITE_DB_PATH')));
 
 
 Artisan::call('migrate');
