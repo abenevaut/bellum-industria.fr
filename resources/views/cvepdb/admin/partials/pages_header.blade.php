18d3dbb2ee151406a747eaccf8066d3345aef656
Antoine Benevaut <antoine@cvepdb.fr>
2016-02-25 13:36:09 +0100

    Suppression de l'utilisation de s3 pour le moment.

--- resources/views/cvepdb/admin/partials/pages_header.blade.php
+++ resources/views/cvepdb/admin/partials/pages_header.blade.php
@@ -27,8 +27,8 @@
 
             <div class="brand inline">
                 <a href="{{ url('admin') }}">
-                    <img src="{{ s3('assets/images/cvepdb/logo.png') }}" alt="logo" data-src="{{ s3('assets/images/cvepdb/logo.png') }}"
-                         data-src-retina="{{ s3('assets/images/cvepdb/logo.png') }}" width="100" height="40">
+                    <img src="/assets/images/cvepdb/logo.png" alt="logo" data-src="/assets/images/cvepdb/logo.png"
+                         data-src-retina="/assets/images/cvepdb/logo.png" width="100" height="40">
                 </a>
             </div>
 
