cd5ebfdb568fa73b39713d16917f1e0aeb13d10c
Antoine Benevaut <antoine@cvepdb.fr>
2016-03-22 21:09:50 +0100

    Progress on adminlte theme.

--- resources/themes/adminlte/views/users/admin/login.blade.php
+++ resources/themes/adminlte/views/users/admin/login.blade.php
@@ -2,13 +2,14 @@
 
 @section('content')
 
-    <p class="login-box-msg">Sign in to start your session</p>
+    <p class="login-box-msg">{{ trans('adminlte::adminlte.auth_introduction') }}</p>
 
     <form action="{{ url('admin/login') }}" method="post">
         {!! csrf_field() !!}
 
         <div class="form-group has-feedback">
-            <input type="email" class="form-control" name="email" value="{{ old('email') }}" placeholder="Email">
+            <input type="email" class="form-control" name="email" value="{{ old('email') }}"
+                   placeholder="{{ trans('adminlte::adminlte.auth_placeholder_email') }}">
             <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
 
             @if ($errors->has('email'))
@@ -19,7 +20,8 @@
         </div>
 
         <div class="form-group has-feedback">
-            <input type="password" class="form-control" name="password" placeholder="Password">
+            <input type="password" class="form-control" name="password"
+                   placeholder="{{ trans('adminlte::adminlte.auth_placeholder_password') }}">
             <span class="glyphicon glyphicon-lock form-control-feedback"></span>
 
             @if ($errors->has('password'))
@@ -34,13 +36,15 @@
             <div class="col-xs-8">
                 <div class="checkbox icheck">
                     <label>
-                        <input type="checkbox" name="remember"> Remember Me
+                        <input type="checkbox" name="remember"> {{ trans('adminlte::adminlte.auth_label_rememberme') }}
                     </label>
                 </div>
             </div>
 
             <div class="col-xs-4">
-                <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
+                <button type="submit" class="btn btn-primary btn-block btn-flat">
+                    {{ trans('adminlte::adminlte.auth_btn_login') }}
+                </button>
             </div>
 
         </div>
