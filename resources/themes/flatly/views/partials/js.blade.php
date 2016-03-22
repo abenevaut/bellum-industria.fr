ba458c23a2564ad7c68acfc510c38ce8f5540253
Antoine Benevaut <antoine@cvepdb.fr>
2016-03-22 11:14:00 +0100

    Add themes\widget google analytics

--- resources/themes/flatly/views/partials/js.blade.php
+++ resources/themes/flatly/views/partials/js.blade.php
@@ -2,3 +2,4 @@
 <script src="{{ asset('themes/flatly/js/bootstrap.min.js') }}"></script>
 <script src="{{ asset('themes/flatly/js/custom.js') }}"></script>
 @yield('js')
+{{--{!! Widget::google_analytics('UA-68419556-2') !!}--}}
--- resources/themes/lumen/views/partials/js.blade.php
+++ resources/themes/lumen/views/partials/js.blade.php
@@ -2,3 +2,4 @@
 <script src="{{ asset('themes/lumen/js/bootstrap.min.js') }}"></script>
 <script src="{{ asset('themes/lumen/js/custom.js') }}"></script>
 @yield('js')
+{{--{!! Widget::google_analytics('UA-68419556-2') !!}--}}
