a0e219de5bb2e0d5ecbc0e26551860706ebb0db5
Antoine Benevaut <antoine@cvepdb.fr>
2016-03-23 12:56:07 +0100

    Use themes module as package.

--- modules/Themes/Resources/lang/en/admin.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'index.title_frontend' => 'Theme frontend list',
-    'index.title_backend' => 'Theme backend list',
-    'index.label.status:active' => 'active',
-    'index.btn.use' => 'Use it',
-];
