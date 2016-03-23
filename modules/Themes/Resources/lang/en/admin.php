23109006615cf60a740f9e46e2415263d41e2c04
Antoine Benevaut <antoine@cvepdb.fr>
2016-03-23 12:53:15 +0100

    Progress on themes module.

---
+++ modules/Themes/Resources/lang/en/admin.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'index.title_frontend' => 'Theme frontend list',
+    'index.title_backend' => 'Theme backend list',
+    'index.label.status:active' => 'active',
+    'index.btn.use' => 'Use it',
+];
--- resources/lang/en/global.php
+++ resources/lang/en/global.php
@@ -10,9 +10,14 @@ return [
     'edit' => 'Edit',
     'remove' => 'Remove',
     'back' => 'Back',
+    'name' => 'Name',
+    'status' => 'Status',
     'last_name' => 'Last name',
     'first_name' => 'First name',
     'role' => 'Role',
     'roles' => 'Roles',
+    'navigation' => 'Navigation',
+    'close' => 'Close',
+    'preview' => 'Preview',
 
 ];
