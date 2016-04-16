1ce499e096d62791267c18bc60be886bfc3439f0
Antoine Benevaut <antoine@cvepdb.fr>
2016-04-16 17:56:34 +0200

    Remove Settings modules.

--- modules/Settings/Http/routes.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-Route::group(['middleware' => ['admin'], 'prefix' => 'admin', 'namespace' => 'Modules\Settings\Http\Controllers'], function () {
-    Route::resource('settings', 'AdminSettingsController');
-});
