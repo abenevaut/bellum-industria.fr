3a48c29a88e6d49d1bbb9da08214f5c059373ab9
Antoine Benevaut <antoine@cvepdb.fr>
2016-04-20 11:08:52 +0200

    Progress on Settings overwriting.

--- core/Http/Controllers/Admin/SettingsController.php
+++ core/Http/Controllers/Admin/SettingsController.php
@@ -1,7 +1,7 @@
 <?php namespace Core\Http\Controllers\Admin;
 
-use CVEPDB\Settings\Facades\Settings;
 use Request;
+use CVEPDB\Settings\Facades\Settings;
 use Core\Http\Controllers\CoreAdminController;
 use Core\Http\Requests\Admin\SettingsGetFormRequest;
 use Core\Http\Requests\Admin\SettingsSetFormRequest;
@@ -20,7 +20,6 @@ class SettingsController extends CoreAdminController
     {
         $data = [];
         $setting_key = $request->get('setting_key');
-
         if (Request::ajax()) {
             $data = [
                 $setting_key => Settings::get($setting_key)
@@ -38,11 +37,8 @@ class SettingsController extends CoreAdminController
         $data = [];
         $setting_key = $request->get('setting_key');
         $setting_value = $request->get('setting_value');
-
         if (Request::ajax()) {
-
             Settings::set($setting_key, $setting_value);
-
             $data = [
                 $setting_key => $setting_value
             ];
