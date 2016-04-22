580460bd751ecc19d51ab402218651671a767454
Antoine Benevaut <antoine@cvepdb.fr>
2016-04-22 19:26:46 +0200

    Progress on Core.

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -10,7 +10,7 @@ $app->instance('request', new \Illuminate\Http\Request);
 $app->make('Illuminate\Contracts\Http\Kernel')->bootstrap();
 
 exec('rm ' . base_path(env('TEST_SQLITE_DB_PATH')));
-exec('cp ' . base_path('TEST_SQLITE_DB_TO_COPY') . ' ' . storage_path(env('TEST_SQLITE_DB_PATH')));
+exec('cp ' . base_path(env('TEST_SQLITE_DB_TO_COPY')) . ' ' . base_path(env('TEST_SQLITE_DB_PATH')));
 
 // Could not be integrated in project because of event function conflict between HOA lib and Laravel
 //exec(base_path("bin/phpmetrics --report-html=tests/_output/phpmetrics-core.html core"));
