e009ef5c511440770383b9216617446cec629c6e
Antoine Benevaut <antoine@cvepdb.fr>
2016-04-25 15:35:51 +0200

    Progress on Core & unit tests.

---
+++ tests/_support/InstallerAcceptanceTester.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Class InstallerAcceptanceTester
+ */
+class InstallerAcceptanceTester extends AcceptanceTester
+{
+    public function trans_field_required()
+    {
+        return trans('installer::installer.error:field_required');
+    }
+
+    public function trans_field_url()
+    {
+        return trans('installer::installer.error:field_url');
+    }
+
+    public function trans_field_email()
+    {
+        return trans('installer::installer.error:field_email');
+    }
+
+    public function trans_password_confirmed()
+    {
+        return trans('installer::installer.error:password_confirmed');
+    }
+
+    public function trans_db_connection()
+    {
+        return trans('installer::installer.error:db_connection');
+    }
+
+    public function trans_field_max($value)
+    {
+        return str_replace(":max", "$value", trans('installer::installer.error:field_max'));
+    }
+
+    public function trans_field_min($value)
+    {
+        return str_replace(":min", "$value", trans('installer::installer.error:field_min'));
+    }
+
+    public function div_error($id)
+    {
+        return "#$id-error";
+    }
+}
--- tests/acceptance/modules/Installer/IndexValidEmptyFormCept.php
+++ tests/acceptance/modules/Installer/IndexValidEmptyFormCept.php
@@ -1,5 +1,5 @@
 <?php 
-$I = new AcceptanceTester($scenario);
+$I = new InstallerAcceptanceTester($scenario);
 $I->wantTo('Test Installer form with empty value');
 
 $faker = Faker\Factory::create();
@@ -32,56 +32,58 @@ $I->fillField('#core_db_password', '');
 
 $I->click('Install');
 
-$I->seeElement('#CORE_SITE_NAME-error');
-$I->see(trans('installer::installer.error:field_required'), '#CORE_SITE_NAME-error');
-$I->dontSee(str_replace(':max', '254', trans('installer::installer.error:field_max')), '#CORE_SITE_NAME-error');
+$I->seeElement($I->div_error('CORE_SITE_NAME'));
+$I->see($I->trans_field_required(), $I->div_error('CORE_SITE_NAME'));
+$I->dontSee($I->trans_field_max(254), $I->div_error('CORE_SITE_NAME'));
 
-$I->seeElement('#CORE_SITE_DESCRIPTION-error');
-$I->see(trans('installer::installer.error:field_required'), '#CORE_SITE_DESCRIPTION-error');
-$I->dontSee(str_replace(':max', '254', trans('installer::installer.error:field_max')), '#CORE_SITE_DESCRIPTION-error');
+$I->seeElement($I->div_error('CORE_SITE_DESCRIPTION'));
+$I->see($I->trans_field_required(), $I->div_error('CORE_SITE_DESCRIPTION'));
+$I->dontSee($I->trans_field_max(254), $I->div_error('CORE_SITE_DESCRIPTION'));
 
-$I->seeElement('#CORE_URL-error');
-$I->see(trans('installer::installer.error:field_required'), '#CORE_URL-error');
-$I->dontSee(str_replace(':max', '254', trans('installer::installer.error:field_max')), '#CORE_URL-error');
-$I->dontSee(trans('installer::installer.error:field_url'), '#CORE_URL-error');
+$I->seeElement($I->div_error('CORE_URL'));
+$I->see($I->trans_field_required(), $I->div_error('CORE_URL'));
+$I->dontSee($I->trans_field_max(254), $I->div_error('CORE_URL'));
+$I->dontSee($I->trans_field_url(), $I->div_error('CORE_URL'));
 
-$I->seeElement('#first_name-error');
-$I->see(trans('installer::installer.error:field_required'), '#first_name-error');
-$I->dontSee(str_replace(':max', '50', trans('installer::installer.error:field_max')), '#first_name-error');
+$I->seeElement($I->div_error('first_name'));
+$I->see($I->trans_field_required(), $I->div_error('first_name'));
+$I->dontSee($I->trans_field_max(50), $I->div_error('first_name'));
 
-$I->seeElement('#last_name-error');
-$I->see(trans('installer::installer.error:field_required'), '#last_name-error');
-$I->dontSee(str_replace(':max', '50', trans('installer::installer.error:field_max')), '#last_name-error');
+$I->seeElement($I->div_error('last_name'));
+$I->see($I->trans_field_required(), $I->div_error('last_name'));
+$I->dontSee($I->trans_field_max(50), $I->div_error('last_name'));
 
-$I->seeElement('#email-error');
-$I->see(trans('installer::installer.error:field_required'), '#email-error');
-$I->dontSee(str_replace(':max', '254', trans('installer::installer.error:field_max')), '#email-error');
-$I->dontSee(trans('installer::installer.error:field_email'), '#email-error');
+$I->seeElement($I->div_error('email'));
+$I->see($I->trans_field_required(), $I->div_error('email'));
+$I->dontSee($I->trans_field_max(254), $I->div_error('email'));
+$I->dontSee($I->trans_field_email(), $I->div_error('email'));
 
-$I->seeElement('#password-error');
-$I->see(trans('installer::installer.error:field_required'), '#password-error');
-$I->dontSee(str_replace(':max', '60', trans('installer::installer.error:field_max')), '#password-error');
-$I->dontSee(str_replace(':min', '6', trans('installer::installer.error:field_min')), '#password-error');
-$I->dontSee(trans('installer::installer.error:password_confirmed'), '#password-error');
+$I->seeElement($I->div_error('password'));
+$I->see($I->trans_field_required(), $I->div_error('password'));
+$I->dontSee($I->trans_field_max(60), $I->div_error('password'));
+$I->dontSee($I->trans_field_min(6), $I->div_error('password'));
+$I->dontSee($I->trans_password_confirmed(), $I->div_error('password'));
 
-$I->seeElement('#password_confirmation-error');
-$I->see(trans('installer::installer.error:field_required'), '#password_confirmation-error');
-$I->dontSee(str_replace(':max', '60', trans('installer::installer.error:field_max')), '#password_confirmation-error');
-$I->dontSee(str_replace(':min', '6', trans('installer::installer.error:field_min')), '#password_confirmation-error');
-$I->dontSee(trans('installer::installer.error:password_confirmed'), '#password_confirmation-error');
+$I->seeElement($I->div_error('password_confirmation'));
+$I->see($I->trans_field_required(), $I->div_error('password_confirmation'));
+$I->dontSee($I->trans_field_max(60), $I->div_error('password_confirmation'));
+$I->dontSee($I->trans_field_min(6), $I->div_error('password_confirmation'));
 
-$I->seeElement('#core_db_host-error');
-$I->see(trans('installer::installer.error:field_required'), '#core_db_host-error');
-$I->dontSee(str_replace(':max', '254', trans('installer::installer.error:field_max')), '#core_db_host-error');
+$I->seeElement($I->div_error('core_db_host'));
+$I->see($I->trans_field_required(), $I->div_error('core_db_host'));
+$I->dontSee($I->trans_field_max(254), $I->div_error('core_db_host'));
 
-$I->seeElement('#core_db_database-error');
-$I->see(trans('installer::installer.error:field_required'), '#core_db_database-error');
-$I->dontSee(str_replace(':max', '254', trans('installer::installer.error:field_max')), '#core_db_database-error');
+$I->seeElement($I->div_error('core_db_database'));
+$I->see($I->trans_field_required(), $I->div_error('core_db_database'));
+$I->dontSee($I->trans_field_max(254), $I->div_error('core_db_database'));
 
-$I->seeElement('#core_db_username-error');
-$I->see(trans('installer::installer.error:field_required'), '#core_db_username-error');
-$I->dontSee(str_replace(':max', '254', trans('installer::installer.error:field_max')), '#core_db_username-error');
+$I->seeElement($I->div_error('core_db_username'));
+$I->see($I->trans_field_required(), $I->div_error('core_db_username'));
+$I->dontSee($I->trans_field_max(254), $I->div_error('core_db_username'));
 
-$I->seeElement('#core_db_password-error');
-$I->see(trans('installer::installer.error:field_required'), '#core_db_password-error');
-$I->dontSee(str_replace(':max', '254', trans('installer::installer.error:field_max')), '#core_db_password-error');
+$I->seeElement($I->div_error('core_db_password'));
+$I->see($I->trans_field_required(), $I->div_error('core_db_password'));
+$I->dontSee($I->trans_field_max(254), $I->div_error('core_db_password'));
+
+$I->dontSeeElement('.callout.callout-danger');
+$I->dontSee($I->trans_db_connection(), '.callout.callout-danger');
