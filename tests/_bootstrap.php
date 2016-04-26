227681a2b6dd90638bacea744a0cf84f2ef66977
Antoine Benevaut <antoine@cvepdb.fr>
2016-04-26 14:42:27 +0200

    Progress on Core & unit tests.

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -2,16 +2,80 @@
 
 // This is global bootstrap for autoloading
 
-require 'bootstrap/autoload.php';
-$app = require 'bootstrap/app.php';
+/*
+ * Run Laravel
+ */
 
-$app->loadEnvironmentFrom('.env.testing');
+require 'bootstrap/autoload.php';
+
+/*
+|--------------------------------------------------------------------------
+| Create The Application
+|--------------------------------------------------------------------------
+|
+| The first thing we will do is create a new Laravel application instance
+| which serves as the "glue" for all the components of Laravel, and is
+| the IoC container for the system binding all of the various parts.
+|
+*/
+
+$app = new Illuminate\Foundation\Application(
+    realpath(__DIR__.'/../')
+);
+
+/*
+|--------------------------------------------------------------------------
+| Bind Important Interfaces
+|--------------------------------------------------------------------------
+|
+| Next, we need to bind some important interfaces into the container so
+| we will be able to resolve them when needed. The kernels serve the
+| incoming requests to this application from both the web and CLI.
+|
+*/
+
+$app->singleton(
+    Illuminate\Contracts\Http\Kernel::class,
+    Core\Http\Kernel::class
+);
+
+$app->singleton(
+    Illuminate\Contracts\Console\Kernel::class,
+    Core\Console\Kernel::class
+);
+
+$app->singleton(
+    Illuminate\Contracts\Debug\ExceptionHandler::class,
+    Core\Exceptions\Handler::class
+);
+
+/*
+|--------------------------------------------------------------------------
+| Detect The Application Environment
+|--------------------------------------------------------------------------
+|
+| Laravel takes a dead simple approach to your application environments
+| so you can just specify a machine name for the host that matches a
+| given environment, then we will automatically detect it for you.
+|
+*/
+
+$app->detectEnvironment(function(){
+    $dotenv = new \Dotenv\Dotenv(__DIR__, '.env.testing');
+    $dotenv->overload(); //this is important
+});
+
 $app->instance('request', new \Illuminate\Http\Request);
 $app->make('Illuminate\Contracts\Http\Kernel')->bootstrap();
+
+/**
+ * Prepare stuff for execution
+ */
 
 // Prepare SQlite DB
 exec('rm ' . base_path(env('TEST_SQLITE_DB_PATH')));
 exec('cp ' . base_path(env('TEST_SQLITE_DB_TO_COPY')) . ' ' . base_path(env('TEST_SQLITE_DB_PATH')));
+
 // Prepare fake .env.installer file
 exec('rm ' . base_path(env('TEST_ENV_INSTALLER_PATH')));
 exec('cp ' . base_path(env('TEST_ENV_INSTALLER_TO_COPY')) . ' ' . base_path(env('TEST_ENV_INSTALLER_PATH')));
