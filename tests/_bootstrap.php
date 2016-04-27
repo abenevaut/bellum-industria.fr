4e41678027da95f319fdc27eba30df97cfb17cae
Antoine Benevaut <antoine@cvepdb.fr>
2016-04-27 15:42:55 +0200

    Progress on Core & unit tests.

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,1 +1,5 @@
 <?php // This is global bootstrap for autoloading
+
+// Could not be integrated in project because of event function conflict between HOA lib and Laravel
+//exec(base_path("bin/phpmetrics --report-html=tests/_output/phpmetrics-core.html core"));
+//exec(base_path("bin/phpmetrics --report-html=tests/_output/phpmetrics-modules.html modules"));
--- tests/acceptance/_bootstrap.php
+++ tests/acceptance/_bootstrap.php
@@ -35,7 +35,3 @@ switch ($settings['l5']['env']['name'])
         break;
     }
 }
-
-// Could not be integrated in project because of event function conflict between HOA lib and Laravel
-//exec(base_path("bin/phpmetrics --report-html=tests/_output/phpmetrics-core.html core"));
-//exec(base_path("bin/phpmetrics --report-html=tests/_output/phpmetrics-modules.html modules"));
