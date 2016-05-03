e128ae89eb7b63aadc3b2313001c07665f4776ea
Antoine Benevaut <antoine@cvepdb.fr>
2016-05-03 18:09:04 +0200

    Progress on Core.

--- resources/views/core/admin/settings/chunks/clouds.blade.php
+++ resources/views/core/admin/settings/chunks/clouds.blade.php
@@ -47,8 +47,7 @@
         </div>
         <div class="form-group form-group-default">
             <label>{{ trans('settings.filesystems_disks_s3_region') }}</label>
-            <input type="text" class="form-control" name="filesystems_disks_s3_region"
-                   value="{{ old('filesystems_disks_s3_region', $settings->get('filesystems.disks.s3.region')) }}">
+            {{ Form::select('filesystems_disks_s3_region', ['us-east-1' => 'US East (N. Virginia)', 'us-west-1' => 'US West (N. California)', 'us-west-2' => 'US West (Oregon)', 'eu-west-1' => 'EU (Ireland)', 'eu-central-1' => 'EU (Frankfurt)', 'ap-northeast-1' => 'Asia Pacific (Tokyo)', 'ap-northeast-2' => 'Asia Pacific (Seoul)', 'ap-southeast-1' => 'Asia Pacific (Singapore)', 'ap-southeast-2' => 'Asia Pacific (Sydney)', 'sa-east-1' => 'South America (São Paulo)'], old('mail_driver', $settings->get('filesystems.disks.s3.region')), ['class' => 'form-control']) }}
         </div>
         <div class="form-group form-group-default">
             <label>{{ trans('settings.filesystems_disks_s3_bucket') }}</label>
