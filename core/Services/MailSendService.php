41b518d26acdeb967277c5b3cb9cdd5bc99e135a
Antoine Benevaut <antoine@cvepdb.fr>
2016-05-03 11:52:29 +0200

    Progress on Core.

---
+++ core/Services/MailSendService.php
@@ -0,0 +1,39 @@
+<?php namespace Core\Services;
+
+use Mail;
+use CVEPDB\Abstracts\Services\Mails\MailService as AbsMailService;
+use CVEPDB\Settings\Facades\Settings;
+
+/**
+ * Class MailSendService
+ * @package Core\Services
+ */
+abstract class MailSendService extends AbsMailService
+{
+	/**
+	 * Send one mail to multiple emails.
+	 *
+	 * @param array $emails All emails to send the message
+	 * @param string $view Blade path view
+	 * @param string $subject Mail subject
+	 * @param array $data Blade template data
+	 * @return mixed
+	 */
+	public function emailTo($emails, $view, $subject, $data = [])
+	{
+		Mail::send($view, $data, function ($message) use ($emails, $subject) {
+
+			$mailfrom = Settings::get('mail.from.address');
+			$mailname = Settings::get('mail.from.name');
+			$mailwatch = Settings::get('core.mail.mailwatch');
+
+			$message->to($emails)
+				->from($mailfrom, $mailname)
+				->subject($subject);
+
+			if (!is_null($mailwatch)) {
+				$message->bcc($mailwatch);
+			}
+		});
+	}
+}
