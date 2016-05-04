5dc3e050058b1b41d7eb4be8eeb59cb5c5255cf5
Antoine Benevaut <antoine@cvepdb.fr>
2016-05-04 11:47:15 +0200

    Progress on Users module.

---
+++ modules/Users/Resources/lang/fr/login.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'frontend_meta_title' => 'User login',
+    'frontend_meta_description' => 'Connect to your dashboard',
+
+    'backend_meta_title' => 'Admin dashboard',
+    'backend_meta_description' => 'Connect to your admin dashboard',
+
+];
---
+++ modules/Users/Resources/lang/fr/register.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+
+    'frontend_meta_title' => 'User registration',
+    'frontend_meta_description' => 'Register to the website',
+
+];
