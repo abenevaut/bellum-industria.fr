e17ea884f24c55ad344e71378dfd96465e1b67cd
Antoine Benevaut <antoine@cvepdb.fr>
2016-05-09 10:38:18 +0200

    Progress on Core.

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,5 +1,10 @@
 <?php // This is global bootstrap for autoloading
 
+define('C3_CODECOVERAGE_ERROR_LOG_FILE', ('/tests/_output/c3_error.log')); //Optional (if not set the default c3 output dir will be used)
+include 'c3.php';
+
+define('MY_APP_STARTED', true);
+
 // Could not be integrated in project because of event function conflict between HOA lib and Laravel
 //exec(base_path("bin/phpmetrics --report-html=tests/_output/phpmetrics-core.html core"));
 //exec(base_path("bin/phpmetrics --report-html=tests/_output/phpmetrics-modules.html modules"));
