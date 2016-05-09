ebe78bbe85b6009cf689fefdfca2c6b75a31dcc7
Antoine Benevaut <antoine@cvepdb.fr>
2016-05-09 16:20:34 +0200

    Progress on Core.

--- deploy.php
+++ deploy.php
@@ -31,54 +31,59 @@ set('writable_dirs', ['bootstrap/cache', 'storage']);
 //    return run('which php.TEST.5')->toString();
 //});
 // Composer local path
-env('bin/composer', function () {
-    $composer = runLocally('which composer')->toString();
+env('bin/composer', function ()
+{
+	$composer = runLocally('which composer')->toString();
 
-    return $composer;
+	return $composer;
 });
 
-task('cvepdb:git', function () {
-    runLocally('git submodule init');
-    runLocally('git submodule update');
+task('cvepdb:git', function ()
+{
+	runLocally('git submodule init');
+	runLocally('git submodule update');
 })->desc('Deploy your project');
 
-task('cvepdb:vendors', function () {
-    upload(env('local_release_path'), env('release_path'));
+task('cvepdb:vendors', function ()
+{
+	upload(env('local_release_path'), env('release_path'));
 })->desc('Deploy your project');
 
-task('local:shared', function () {
+task('local:shared', function ()
+{
 
-    $remoteSharedPath = '{{deploy_path}}/shared';
-    $localSharedPath = '{{local_deploy_path}}/shared';
+	$remoteSharedPath = '{{deploy_path}}/shared';
+	$localSharedPath = '{{local_deploy_path}}/shared';
 
-    foreach (get('shared_files') as $file) {
-        // Current file directory path
-        $dirname = dirname($file);
-        // Remove from source.
-        run("if [ -f $(echo {{release_path}}/$file) ]; then rm -rf {{release_path}}/$file; fi");
-        // Ensure dir is available in release
-        run("if [ ! -d $(echo {{release_path}}/$dirname) ]; then mkdir -p {{release_path}}/$dirname;fi");
-        // Create dir of shared file
-        run("mkdir -p $remoteSharedPath/".$dirname);
-        // Send local shared file to remote shared directory
-        upload("$localSharedPath/$file", "$remoteSharedPath/$file");
-        // Symlink shared dir to release dir
-        run("ln -nfs $remoteSharedPath/$file {{release_path}}/$file");
-    }
+	foreach (get('shared_files') as $file)
+	{
+		// Current file directory path
+		$dirname = dirname($file);
+		// Remove from source.
+		run("if [ -f $(echo {{release_path}}/$file) ]; then rm -rf {{release_path}}/$file; fi");
+		// Ensure dir is available in release
+		run("if [ ! -d $(echo {{release_path}}/$dirname) ]; then mkdir -p {{release_path}}/$dirname;fi");
+		// Create dir of shared file
+		run("mkdir -p $remoteSharedPath/" . $dirname);
+		// Send local shared file to remote shared directory
+		upload("$localSharedPath/$file", "$remoteSharedPath/$file");
+		// Symlink shared dir to release dir
+		run("ln -nfs $remoteSharedPath/$file {{release_path}}/$file");
+	}
 })->desc('Creating symlinks for shared files');
 
 task('deploy', [
-    'local:prepare',
-    'deploy:prepare',
-    'local:release',
-    'deploy:release',
-    'local:update_code',
-    'local:vendors',
-    'local:shared',
-    //'deploy:writable',
-    'deploy:symlink',
-    'cleanup',
-    'local:cleanup',
+	'local:prepare',
+	'deploy:prepare',
+	'local:release',
+	'deploy:release',
+	'local:update_code',
+	'local:vendors',
+	'local:shared',
+	//'deploy:writable',
+	'deploy:symlink',
+	'cleanup',
+	'local:cleanup',
 ])->desc('Deploy your project');
 
 after('local:update_code', 'cvepdb:git');
