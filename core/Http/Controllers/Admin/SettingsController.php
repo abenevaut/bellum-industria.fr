9ed2238247c767c81258a4f451346a48b11fe02a
Antoine Benevaut <antoine@cvepdb.fr>
2016-05-11 16:17:24 +0200

    Progress on Core and Tests.

--- core/Http/Controllers/Admin/SettingsController.php
+++ core/Http/Controllers/Admin/SettingsController.php
@@ -2,6 +2,8 @@
 
 use Core\Http\Controllers\CoreAdminController;
 use Core\Http\Outputters\Admin\SettingsOutputter;
+use Core\Http\Requests\Admin\SettingsGetFormRequest;
+use Core\Http\Requests\Admin\SettingsSetFormRequest;
 use Core\Http\Requests\Admin\SettingsStoreFormRequest;
 
 /**
@@ -10,36 +12,57 @@ use Core\Http\Requests\Admin\SettingsStoreFormRequest;
  */
 class SettingsController extends CoreAdminController
 {
-
-	/**
-	 * @var SettingsOutputter|null
-	 */
-	protected $outputter = null;
+    /**
+     * @var SettingsOutputter|null
+     */
+    protected $outputter = null;
 
 	/**
 	 * SettingsController constructor.
 	 *
 	 * @param SettingsOutputter $outputter
 	 */
-	public function __construct(SettingsOutputter $outputter)
-	{
-		$this->outputter = $outputter;
-	}
+    public function __construct(SettingsOutputter $outputter)
+    {
+		parent::__construct();
+        $this->outputter = $outputter;
+    }
 
-	/**
-	 * @return mixed
-	 */
-	public function index()
-	{
-		return $this->outputter->index();
-	}
+    /**
+     * @return mixed
+     */
+    public function index()
+    {
+        return $this->outputter->index();
+    }
 
-	/**
-	 * @return mixed
-	 */
-	public function store(SettingsStoreFormRequest $request)
-	{
-		return $this->outputter->store($request);
-	}
+    /**
+     * @return mixed
+     */
+    public function store(SettingsStoreFormRequest $request)
+    {
+        return $this->outputter->store($request);
+    }
 
+    /**
+     * Ajax method to get settings
+     *
+     * @param SettingsGetFormRequest $request
+     * @return array
+     */
+    public function get(SettingsGetFormRequest $request)
+    {
+        return $this->outputter->get($request);
+    }
+
+    /**
+     * Ajax method to set settings
+     *
+     * @param SettingsSetFormRequest $request
+     * @return array
+     */
+    public function set(SettingsSetFormRequest $request)
+    {
+        return $this->outputter->set($request);
+    }
 }
--- core/Http/Outputters/Admin/SettingsOutputter.php
+++ core/Http/Outputters/Admin/SettingsOutputter.php
@@ -1,5 +1,7 @@
 <?php namespace Core\Http\Outputters\Admin;
 
+use Request;
+use Response;
 use Core\Http\Outputters\AdminOutputter;
 use Core\Domain\Settings\Repositories\SettingsRepository;
 use CVEPDB\Abstracts\Http\Requests\FormRequest as AbsFormRequest;
@@ -63,6 +65,44 @@ class SettingsOutputter extends AdminOutputter
 		}
 
 		return $this->redirectTo('admin/settings');
+	}
+
+	/**
+	 * @param AbsFormRequest $request
+	 *
+	 * @return mixed
+	 */
+	public function get(AbsFormRequest $request)
+	{
+		$data = [];
+
+		if (Request::ajax())
+		{
+			$setting_key = $request->get('setting_key');
+			$data[$setting_key] = $this->r_settings->get($setting_key);
+		}
+
+		return Response::json($data);
+	}
+
+	/**
+	 * @param AbsFormRequest $request
+	 *
+	 * @return mixed
+	 */
+	public function set(AbsFormRequest $request)
+	{
+		$data = [];
+		
+		if (Request::ajax())
+		{
+			$setting_key = $request->get('setting_key');
+			$setting_value = $request->get('setting_value');
+			$this->r_settings->set($setting_key, $setting_value);
+			$data[$setting_key] = $setting_value;
+		}
+
+		return Response::json($data);
 	}
 
 	/**
