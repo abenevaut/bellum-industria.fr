2e9aca9c37424be4aff7b102b826e1a56dd60322
Antoine Benevaut <antoine@cvepdb.fr>
2016-05-19 15:18:39 +0200

    Progress on Core. Remove npm / gulp tools to localize it directly in themes for dev mode.

--- core/Domain/Users/Repositories/UserRepository.php
+++ core/Domain/Users/Repositories/UserRepository.php
@@ -1,12 +1,12 @@
 <?php namespace Core\Domain\Users\Repositories;
 
-use CVEPDB\Domain\Users\Repositories\UserRepository as Repository;
+use CVEPDB\Domain\Users\Repositories\UserRepositoryInterface as RepositoryInterface;
 
 /**
  * Interface UserRepository
  * @package Core\Domain\Users\Repositories
  */
-interface UserRepository extends Repository
+interface UserRepository extends RepositoryInterface
 {
     
 }
