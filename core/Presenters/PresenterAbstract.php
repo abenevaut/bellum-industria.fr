91fd8b7500ca704e7fb322fd4cdbbac7888fcacc
Antoine Benevaut <antoine@cvepdb.fr>
2016-07-07 21:27:28 +0200

    Progress on code generators.

---
+++ core/Presenters/PresenterAbstract.php
@@ -0,0 +1,11 @@
+<?php namespace Core\Presenters;
+
+use Prettus\Repository\Presenter\FractalPresenter;
+
+/**
+ * Class PresenterAbstract
+ * @package Core\Presenters
+ */
+abstract class PresenterAbstract extends FractalPresenter
+{
+}
