4af6499bc0da5e83beab70bb7b273d45806689be
Antoine Benevaut <antoine@cvepdb.fr>
2016-07-09 16:53:23 +0200

    Avancement sur le site multigaming.

--- resources/views/app/emails/partials/clip_signature.blade.php
+++
@@ -1,8 +0,0 @@
-<tr>
-    <td height="10"></td>
-</tr>
-<tr>
-    <td align="left" style="font-family: 'Open Sans', Arial, sans-serif; font-size:13px; color:#999999; line-height:28px;">
-        {{ trans('emails/signature.signature') }}
-    </td>
-</tr>
