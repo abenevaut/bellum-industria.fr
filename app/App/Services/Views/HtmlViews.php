519d49678b5ef57fcbb3b383194c1ce7964318dd
Antoine Benevaut <antoine@cvepdb.fr>
2016-08-20 14:01:35 +0200

    DDD changements.

---
+++ app/App/Services/Views/HtmlViews.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace cms\App\Services\Views;
+
+use cms\App\Services\Views\ViewsInterface;
+
+/**
+ * Class HtmlViews
+ * @package cms\App\Services\Views
+ */
+class HtmlViews implements ViewsInterface
+{
+
+	protected $view_prefix = null;
+
+	protected $current_module = null;
+
+	public function setViewPrefix($view_prefix)
+	{
+		$this->view_prefix = $view_prefix;
+	}
+
+	public function setCurrentModule($current_module)
+	{
+		$this->current_module = $current_module;
+	}
+
+	public function output($view, $data = [])
+	{
+		return cmsview(
+			$view,
+			$data,
+			$this->view_prefix,
+			$this->current_module
+		);
+	}
+}
