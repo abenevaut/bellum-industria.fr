3d0c06251eb2266e06477a7f0d87693a3838def0
Antoine Benevaut <antoine@cvepdb.fr>
2016-09-03 21:44:34 +0200

    Update addresses package.

---
+++ database/migrations/2016_08_31_174618_create_countries_tables.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCountriesTables extends Migration
+{
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('countries', function (Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('name', 60);
+			$table->string('slug', 60);
+			$table->string('iso_3166_2', 13);
+			$table->string('iso_3166_alpha_2', 2);
+			$table->string('iso_3166_alpha_3', 3);
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('countries');
+	}
+
+}
