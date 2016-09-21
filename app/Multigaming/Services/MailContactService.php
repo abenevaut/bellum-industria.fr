17b9712e91b7c6a73867b92ffcb131dd6d0f0b43
Antoine Benevaut <antoine@cvepdb.fr>
2016-09-21 12:34:54 +0200

    Progress on site.

--- app/Multigaming/Services/MailContactService.php
+++ app/Multigaming/Services/MailContactService.php
@@ -1,13 +1,13 @@
 <?php namespace cms\Multigaming\Services;
 
-use cms\App\Services\Mails\MailQueueService;
+use cms\App\Services\Mails\MailSendService;
 use cms\Vitrine\Repositories\LogContact;
 
 /**
  * Class MailContactService
  * @package cms\Multigaming\Services
  */
-class MailContactService extends MailQueueService
+class MailContactService extends MailSendService
 {
 
 	/**
