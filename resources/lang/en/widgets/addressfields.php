e81af6ddb283e5cf10b520a196f685de80ccfe9c
Antoine Benevaut <antoine@cvepdb.fr>
2016-09-22 11:40:52 +0200

    Progress on addresses management.

---
+++ resources/lang/en/widgets/addressfields.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+	'country_address'                => 'Country',
+	'state_or_province_address'      => 'State or province',
+	'substate_address'               => 'Region',
+	'street_address'                 => 'Address',
+	'city_or_town_address'           => 'City or town',
+	'zipcode_or_postal_code_address' => 'Zip code',
+];
--- resources/views/app/widgets/addressfields.blade.php
+++ resources/views/app/widgets/addressfields.blade.php
@@ -167,7 +167,7 @@
 
 @if (config('addresses.show_country') && $show_country)
 	<div class="form-group">
-		{!! Form::label($prefix . '_address_country', trans('global.country_address'), ['class'=>'col-sm-3 control-label']) !!}
+		{!! Form::label($prefix . '_address_country', trans('widgets/addressfields.country_address'), ['class'=>'col-sm-3 control-label']) !!}
 		<div class="col-sm-9">
 			{!! Addresses::selectCountry(
 				$prefix . '_address_country',
@@ -183,7 +183,7 @@
 
 @if (config('addresses.show_state') && $show_state)
 	<div class="form-group">
-		{!! Form::label($prefix . '_address_state_id', trans('global.state_or_province_address'), ['class'=>'col-sm-3 control-label']) !!}
+		{!! Form::label($prefix . '_address_state_id', trans('widgets/addressfields.state_or_province_address'), ['class'=>'col-sm-3 control-label']) !!}
 		<div class="col-sm-9">
 			<select id="{{ $prefix }}_address_state_id" class="form-control" name="{{ $prefix }}_address_state_id"
 					data-value="{{ old($prefix . '_address_state_id', $address['state_id']) }}"
@@ -194,7 +194,7 @@
 @endif
 
 <div class="form-group">
-	{!! Form::label($prefix . '_address_substate_id', trans('global.substate_address'), ['class'=>'col-sm-3 control-label']) !!}
+	{!! Form::label($prefix . '_address_substate_id', trans('widgets/addressfields.substate_address'), ['class'=>'col-sm-3 control-label']) !!}
 	<div class="col-sm-9">
 		<select id="{{ $prefix }}_address_substate_id" class="form-control" name="{{ $prefix }}_address_substate_id"
 				data-value="{{ old($prefix . '_address_substate_id', $address['substate_id']) }}"
@@ -204,7 +204,7 @@
 </div>
 
 <div class="form-group">
-	{!! Form::label($prefix . '_address_street', trans('global.street_address'), ['class'=>'col-sm-3 control-label']) !!}
+	{!! Form::label($prefix . '_address_street', trans('widgets/addressfields.street_address'), ['class'=>'col-sm-3 control-label']) !!}
 	<div class="col-sm-9">
 		{!! Form::text($prefix . '_address_street', old($prefix . '_address_street', $address['street']), ['class'=>'form-control']) !!}
 		{!! Form::text($prefix . '_address_street_extra', old($prefix . '_address_street_extra', $address['street_extra']), ['class'=>'form-control', 'style'=>'margin-top:6px;']) !!}
@@ -212,11 +212,11 @@
 </div>
 
 <div class="form-group">
-	{!! Form::label($prefix . '_address_city', trans('global.city_or_town_address'), ['class'=>'col-sm-3 control-label']) !!}
+	{!! Form::label($prefix . '_address_city', trans('widgets/addressfields.city_or_town_address'), ['class'=>'col-sm-3 control-label']) !!}
 	<div class="col-sm-4">
 		{!! Form::text($prefix . '_address_city', old($prefix . '_address_city', $address['city']), ['class'=>'form-control']) !!}
 	</div>
-	{!! Form::label($prefix . '_address_zip', trans('global.zipcode_or_postal_code_address'), ['class'=>'col-sm-2 control-label']) !!}
+	{!! Form::label($prefix . '_address_zip', trans('widgets/addressfields.zipcode_or_postal_code_address'), ['class'=>'col-sm-2 control-label']) !!}
 	<div class="col-sm-3">
 		{!! Form::text($prefix . '_address_zip', old($prefix . '_address_zip', $address['zip']), ['class'=>'form-control']) !!}
 	</div>
