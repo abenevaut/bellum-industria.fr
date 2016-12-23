9699a17619466aa072d529a997733860854466f9
Antoine Benevaut <antoine@cvepdb.fr>
2016-12-23 22:14:02 +0100

    Progress on CMS.

--- config/sentry.php
+++ config/sentry.php
@@ -1,6 +1,6 @@
 <?php
 return array(
-	'dsn' => env('SENTRY_DSN'),
+	'dsn' => env('SENTRY_DSN', null),
 	// capture release as git sha
 	// 'release' => trim(exec('git log --pretty="%h" -n1 HEAD')),
 );
