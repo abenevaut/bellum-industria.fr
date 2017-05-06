405d809163716304efed77ad81948861787f9646
Antoine Benevaut <antoine@cvepdb.fr>
2017-05-06 18:17:58 +0200

    Progress on site.

--- resources/views/frontend/home/index.blade.php
+++ resources/views/frontend/home/index.blade.php
@@ -1,5 +1,64 @@
 @extends('frontend.layouts.default')
 
+@section('css')
+@endsection
+
+@section('js')
+	<script src="plugins/masonry/imagesloaded.pkgd.min.js"></script>
+	<script src="plugins/masonry/masonry.pkgd.min.js"></script>
+	<script>
+		(function ($) {
+			"use strict";
+			var $container = $('.masonry');
+			$($container).imagesLoaded(function () {
+				$($container).masonry({
+					itemSelector: '.post-grid',
+					columnWidth: '.post-grid'
+				});
+			});
+			$(document).ready(function() {
+
+				/**
+				 * Youtube
+				 */
+
+				var $ytb_background = $('.youtube-background');
+
+				$.ajax({
+					type: "GET",
+					url: "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId={{ env('BI_YOUTUBE_CHANNEL_ID') }}&maxResults=1&order=date&type=video&key={{ env('BI_GOOGLE_API_KEY_JS') }}",
+					cache: false,
+					dataType: 'jsonp',
+					success: function (data) {
+						var last_video = data.items[0];
+						$ytb_background.css('background-image', 'url(https://img.youtube.com/vi/' + last_video.id.videoId + '/maxresdefault.jpg)');
+						$ytb_background.find('.embed-responsive-item').attr('src', 'https://www.youtube.com/embed/' + last_video.id.videoId + '?rel=0&amp;showinfo=0');
+						$ytb_background.show();
+					},
+					error: function() {
+						$ytb_background.hide();
+					}
+				});
+
+				/**
+				 * Twitch
+				 */
+
+//				$.ajax({
+//					url: 'https://api.twitch.tv/kraken/streams/bellumindustria',
+//					dataType: 'jsonp',
+//					success: function (channel) {
+//						console.log(channel);
+//					},
+//					error: function () {
+//						//request failed
+//					}
+//				});
+			});
+		})(jQuery);
+	</script>
+@endsection
+
 @section('content')
 
 	<section class="border-bottom-1 border-grey-300 padding-10">
