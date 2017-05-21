fe0c2a45e7d120cae464a6cde4374123a09f6a38
Antoine Benevaut <antoine@cvepdb.fr>
2017-05-21 09:35:32 +0200

    Set all favicon

--- resources/views/frontend/partials/default/metadatas.blade.php
+++ resources/views/frontend/partials/default/metadatas.blade.php
@@ -3,7 +3,12 @@
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-<link rel="shortcut icon" href="/img/favicon.ico">
+<link rel="shortcut icon" type="image/x-icon" href="{{ asset('/img/favicon.ico') }}?v1"/>
+<link href="{{ asset('/img/apple-touch-icon.png') }}?v1" rel="apple-touch-icon" type="image/x-icon" />
+<link href="{{ asset('/img/apple-touch-icon-precomposed.png') }}?v1" rel="apple-touch-icon" type="image/x-icon" />
+<link href="{{ asset('/img/apple-touch-icon-57x57-precomposed.png') }}?v1" rel="apple-touch-icon" type="image/x-icon" />
+<link href="{{ asset('/img/apple-touch-icon-72x72-precomposed.png') }}?v1" rel="apple-touch-icon" type="image/x-icon" />
+<link href="{{ asset('/img/apple-touch-icon-114x114-precomposed.png') }}?v1" rel="apple-touch-icon" type="image/x-icon" />
 <title>@section('title', 'www.bellum-industria.fr')</title>
 <meta name="description" content="www.bellum-industria.fr">
 <meta name="author" content="Antoine Benevaut <antoine@bellum-industria.fr>">
