ce508bc1eddeecbbf91ebdaf6621b74ff89a5979
Antoine Benevaut <antoine@cvepdb.fr>
2017-06-18 21:50:58 +0200

    Progress on site.

--- deploy.php
+++ deploy.php
@@ -32,22 +32,25 @@ require 'recipe/laravel.php';
 	'storage/logs',
 ]);
 
-\Deployer\task('bellumindustriafr:build-docker', function() {
+\Deployer\task('bellumindustriafr:docker:build', function() {
 	\Deployer\run("docker-compose --project-name bellumindustria -f {{current_path}}/docker-compose.yml build apache2 php-fpm php-worker mysql redis");
 });
 
-\Deployer\task('bellumindustriafr:start-docker', function() {
+\Deployer\task('bellumindustriafr:docker:start', function() {
 	\Deployer\run("docker-compose --project-name bellumindustria -f {{current_path}}/docker-compose.yml up -d apache2 php-fpm php-worker mysql redis");
 });
 
-\Deployer\task('bellumindustriafr:composer-docker', function() {
+\Deployer\task('bellumindustriafr:docker:composer-install', function() {
 	\Deployer\run("docker-compose --project-name bellumindustria -f {{current_path}}/docker-compose.yml exec -T workspace composer install");
-	\Deployer\run("docker-compose --project-name benevaut -f {{current_path}}/docker-compose.yml exec -T workspace php artisan optimize");
-	\Deployer\run("docker-compose --project-name benevaut -f {{current_path}}/docker-compose.yml exec -T workspace php artisan route:cache");
-	\Deployer\run("docker-compose --project-name benevaut -f {{current_path}}/docker-compose.yml exec -T workspace php artisan config:cache");
 });
 
-\Deployer\task('bellumindustriafr:migrate-docker', function() {
+\Deployer\task('bellumindustriafr:docker:artisan-optimize', function() {
+	\Deployer\run("docker-compose --project-name benevaut -f {{current_path}}/docker-compose.yml exec -T workspace php /var/www/artisan optimize");
+	\Deployer\run("docker-compose --project-name benevaut -f {{current_path}}/docker-compose.yml exec -T workspace php /var/www/artisan route:cache");
+	\Deployer\run("docker-compose --project-name benevaut -f {{current_path}}/docker-compose.yml exec -T workspace php /var/www/artisan config:cache");
+});
+
+\Deployer\task('bellumindustriafr:docker:artisan-migrate', function() {
 	\Deployer\run("docker-compose --project-name bellumindustria -f {{current_path}}/docker-compose.yml exec -T workspace php /var/www/artisan migrate");
 });
 
@@ -69,10 +72,11 @@ require 'recipe/laravel.php';
 //	'artisan:optimize',
 	'deploy:clear_paths',
 	'deploy:symlink',
-	'bellumindustriafr:build-docker',
-	'bellumindustriafr:start-docker',
-	'bellumindustriafr:composer-docker',
-	'bellumindustriafr:migrate-docker',
+	'bellumindustriafr:docker:build',
+	'bellumindustriafr:docker:start',
+	'bellumindustriafr:docker:composer-install',
+	'bellumindustriafr:docker:artisan-optimize',
+	'bellumindustriafr:docker:artisan-migrate',
 	'deploy:unlock',
 	'cleanup',
 	'success'
